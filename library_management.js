887fd96b552adc2f0dc9a9bc46d7db511382b36b
AdityaSharma42 <adityasharma3@usf.edu>
2024-10-20 21:46:15 -0400

    Calculate total available books in the section

--- library_management.js
+++ library_management.js
@@ -1,19 +1,20 @@
 //Task 1
 class Book {
+    #isAvailable;
     constructor(title, author, ISBN) {
         this.title= title;
         this.author= author;
         this.ISBN= ISBN;
-        this._isAvailable= true;  
+        this.#isAvailable= true;  
     }
     getDetails(){
         return `The title of the book is ${this.title},and it was written by ${this.author}. The ISBN of the book is ${this.ISBN}.`;
     }
     get isAvailable(){
-        return this._isAvailable;
+        return this.#isAvailable;
     }
     set isAvailable(status){
-        this._isAvailable= status;
+        this.#isAvailable= status;
     }
 }
 //Task 2 AND Task 5
@@ -23,13 +24,13 @@ class Section {
         this.books=[];
     }
     addBook(book){
-        this.books.push(book)
+        this.books.push(book);
     }
     getAvailableBooks(){
         return this.books.filter(book=> book.isAvailable).length;
     }
     listBooks(){
-        return this.books.map(book=> `${book.getDetails()}, Available: ${book._isAvailable}`);
+        return this.books.map(book=> `${book.getDetails()}, Available: ${book.isAvailable}`).join(', ');
     }
     calculateTotalBooksAvailable(){
         return this.getAvailableBooks();
@@ -43,18 +44,18 @@ class Patron{
     }
     borrowBook(book){
         if (book.isAvailable){
-            book.isAvailable= true;
+            book.isAvailable= false;
             this.borrowedBooks.push(book);
-            console.log (`${this.name} borrowed: ${book.title}`);
+            console.log (`${this.name} borrowed ${book.title}`);
         } else {
-            console.log (`${book.title} has already been borrowed and is not available.`);
+            console.log (`${this.name} could not borrow ${book.title}because it is borrowed by a VIP patron.`);
         }
     }
     returnBook(book){
         if (this.borrowedBooks.includes(book)){
             book.isAvailable=true;
             this.borrowedBooks= this.borrowedBooks.filter(b=> b!==book);
-            console.log(`${this.name}returned: ${book.title}`);
+            console.log(`${this.name} returned ${book.title}`);
 
         }else{
             console.log (`${this.name} does not have ${book.title}.`);
@@ -66,14 +67,19 @@ class Patron{
 class VIPPatron extends Patron{
     constructor(name,){
         super (name);
-        this.priority=true;
+        
     }
    borrowBook(book){
-    super.borrowBook(book);
-    if (book.isAvailable===false){
-        console.log(`${this.name} has priority`);
+    if (book.isAvailable){
+        book.isAvailable=false;
+        this.borrowedBooks.push(book);
+        console.log(`${this.name} has priority and borrowed ${book.title}.`);
+    }else{
+        console.log(`${book.title} is currently borrowed as ${this.name} also borrowed the book and gets it due to priority.`);
+       
+        this.borrowedBooks.push(book);
+        
     }
     }
 }
-
 
